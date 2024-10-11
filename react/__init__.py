e01469aa7ea25e64ee6f4a67171d2e8caac96b64
SadokBarbouche <sadokchameseddine.barbouche@insat.ucar.tn>
2024-10-12 00:40:13 +0100

    basic chatbot with langgraph

--- react/__init__.py
+++ react/__init__.py
@@ -1,5 +1,6 @@
 from .react_agent import Agent
 from .react_prompt import prompt
 from .tools import calculate, average_dog_weight
+from .model import get_client
 
-__all__ = ["Agent", "prompt", "calculate", "average_dog_weight"]
+__all__ = ["Agent", "prompt", "calculate", "average_dog_weight", "get_client"]
